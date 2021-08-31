dfc8c133ce871597f2fef9295dcd1e70abc39f26
cyp10 <chethanayp18@gmail.com>
2021-08-31 19:46:17 +0530

    first commit

---
+++ app/src/main/java/android/example/wallpaperapp/MainActivity.java
@@ -0,0 +1,69 @@
+package android.example.wallpaperapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    Button changewallpaper;
+    Timer mytimer;
+    Drawable drawable;
+    WallpaperManager wpm;
+    int prev=1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mytimer = new Timer();
+        wpm = WallpaperManager.getInstance(this);
+        changewallpaper = findViewById(R.id.button);
+        changewallpaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setWallpaper();
+            }
+        });
+    }
+    private void setWallpaper() {
+        mytimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(prev==1) {
+                    drawable = getResources().getDrawable(R.drawable.img1);
+                    prev = 2;
+                }
+                else if(prev==2) {
+                    drawable = getResources().getDrawable(R.drawable.img2);
+                    prev=3;
+                }
+                else if(prev==3) {
+                    drawable = getResources().getDrawable(R.drawable.img3);
+                    prev=4;
+                }
+                else if(prev==4) {
+                    drawable = getResources().getDrawable(R.drawable.ig4);
+                    prev=5;
+                }
+                else if(prev==5) {
+                    drawable = getResources().getDrawable(R.drawable.img5);
+                    prev=1;
+                }
+                Bitmap wallpaper = ((BitmapDrawable)drawable).getBitmap();
+                try {
+                    wpm.setBitmap(wallpaper);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        },0,3000); }
+}
